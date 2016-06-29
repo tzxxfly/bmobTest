c64602be878305d6c2c776cbecaeceefcfaf1777
tzxxfly <tzxxfly@sohu.com>
2016-06-29 21:36:13 +0800

    firstCommit

---
+++ cloud/xf.js
@@ -0,0 +1,66 @@
+/**
+ * Created by xfly on 2016/6/29.
+ */
+function onRequest(request, response, modules) {
+    // var db = modules.oData;
+    //
+    // db.find({
+    //     "table":"activity"
+    // },function(err,data){
+    //     // var leagueTeamsObject = JSON.parse(data);
+    //     //回调函数
+    //     checkAddress(data);
+    //     //response.write(data);
+    // });
+    //获取数据库对象
+
+    //获取Posts表中的所有值
+    /*    db.find({
+     "table":"activity"
+     },function(err,data){
+     response.write(data || err);
+     });*/
+
+    //获取数据库对象
+    var db = modules.oData;
+    //获取Posts表中的所有值
+    db.find({
+        "table":"activity",          //表名
+        "keys":"username,type",         //返回字段列表，多个字段用,分隔
+        "where":{"objectId":"FTOLCCCD"},       //查询条件是一个JSON object
+        //"order":"-a,b",         //排序列表，[-]字段名称,-表示降序，默认为升序
+        "limit":10,            //limit大小，一页返回多少条记录，默认为0
+        //"skip":2,             //skip,分页offset，(page-1)*limit
+        "count":4            //count,只返回符合条件的记录总数
+    },function(err,data){
+        response.write(JSON.stringify(data));
+    });
+
+
+
+    function jsonTest() {
+        var name = request.body.name;
+        var jsonObject = {};
+        var list = [];
+        list[list.length] = 1;
+        list[list.length] = 2;
+
+        jsonObject.a = "bbb";
+        jsonObject.b = "cccc";
+        var jsonSubObject = {};
+        jsonSubObject.c = "mmm";
+        jsonSubObject.d = "eee";
+        jsonObject.subobject = jsonSubObject;
+        jsonObject.list = list;
+        jsonObject.name = name;
+        response.end(JSON.stringify(jsonObject));
+    }
+
+    //jsonTest();
+
+    function checkAddress(jsonStr) {
+        var leagueTeamsObject = JSON.parse(jsonStr);
+        response.write(leagueTeamsObject.results[0].address);
+    }
+}
+exports.xf = onRequest;
---
+++ test/test_xf.js
@@ -0,0 +1,118 @@
+var tool = require("bmobcloud-local");
+
+//请根据自己的情况修改application_id和rest_key信息
+var options = require("../AppConfig.json");
+
+tool.initialize(options.app_key, options.rest_key);
+
+function local_xf() {
+//调用hello.js云端代码
+    var first = require("../cloud/xf.js").xf;
+    tool.test(first, {"name": "Bmob后端云"});
+}
+local_xf();
+
+function local() {
+//调用hello.js云端代码
+    var hello = require("../cloud/hello.js").hello;
+    tool.test(hello);
+
+//调用first.js云端代码（带上传参数）
+    var first = require("../cloud/first.js").first;
+    tool.test(first, {"name": "Bmob后端云"});
+
+//调用second.js云端代码
+    var second = require("../cloud/second.js").second;
+    tool.test(second);
+
+//调用third.js云端代码（带上传参数）
+    var third = require("../cloud/third.js").third;
+    tool.test(third, {"userid": "y6qBDvXj"});
+}
+
+//local();
+
+function server() {
+    var path = require("path");
+//调用first.js云端代码（带上传参数）
+    tool.testInServer(path.resolve(__dirname, "../cloud/first.js"), {"name": "Bmob后端云"});
+
+//调用second.js云端代码
+    tool.testInServer(path.resolve(__dirname, "../cloud/second.js"));
+
+//调用third.js云端代码（带上传参数）
+    tool.testInServer(path.resolve(__dirname, "../cloud/third.js"), {"userid": "y6qBDvXj"});
+}
+
+//server();
+
+function relation() {
+    var leagueScoreTable = require("../cloud/leagueScoreTable.js").leagueScoreTable;
+    tool.test(leagueScoreTable);
+}
+
+function bug2() {
+    var playerScore = require("../cloud/playerScore.js").playerScore;
+    tool.test(playerScore);
+}
+
+//relation();
+//bug2();
+
+function testUserSignUp() {
+    var userSignUp = require("../cloud/userSignUp.js").userSignUp;
+    tool.test(userSignUp);
+}
+
+//testUserSignUp();
+
+function testUserLogin() {
+    var testLogin = require("../cloud/testLogin.js").testLogin;
+    tool.test(testLogin);
+}
+
+//testUserLogin();
+
+function testUpdateUserByObjectId() {
+    var testUpdateUserByObjectId = require("../cloud/testUpdateUserByObjectId.js").testUpdateUserByObjectId;
+    tool.test(testUpdateUserByObjectId);
+}
+
+//testUpdateUserByObjectId();
+
+function testRemoveUserByObjectId() {
+    var testRemoveUserByObjectId = require("../cloud/testRemoveUserByObjectId.js").testRemoveUserByObjectId;
+    tool.test(testRemoveUserByObjectId);
+}
+
+//testRemoveUserByObjectId();
+
+function testWinxinVerify() {
+    var weixin = require("../cloud/weixin.js").weixin;
+    // GET /messages?signature=f3b3ed8c9d4884df62e12b3dfba1f9240713bccf&echostr=1059940013166034888&timestamp=1446001536&nonce=712903944 200 2.133 ms - 19
+    tool.test(weixin,
+        {
+            query: "signature=f3b3ed8c9d4884df62e12b3dfba1f9240713bccf&echostr=1059940013166034888&timestamp=1446001536&nonce=712903944",
+            method: "GET"
+        }
+    );
+}
+
+//testWinxinVerify();
+
+var cs = require("coffee-script");
+var wxd = require("./weixin_sample")
+function testWinxinText() {
+    var weixin = require("../cloud/weixin.js").weixin;
+    tool.testWithHeaders(weixin,
+        {
+            type: "XML",
+            data: wxd.text
+        },
+        {
+            'content-type': 'text/xml'
+        }
+    );
+}
+
+//testWinxinText();
